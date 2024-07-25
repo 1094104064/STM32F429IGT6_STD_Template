99aadb6649b26eeb4e919ea95c1289465d7880f8
GarMing <1094104064@qq.com>
2024-07-25 20:59:26 +0800

    template project

---
+++ User/Basic/main.c
@@ -0,0 +1,100 @@
+/**
+  ******************************************************************************
+  *
+  * @file    main.c
+  * @author  GarMing
+  * @brief   Template Project
+  *
+  ******************************************************************************
+  * @attention
+  *
+  *
+  ******************************************************************************
+  **/
+
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+static void Init(void);
+static void Config(void);
+static void Loop(void);
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/ 
+
+/**
+  * @brief  Program entrance.
+  * @param  None
+  * @retval 0 Exit status of the program
+  */
+int main(void)
+{	
+    Init();
+    Config();
+    Loop();
+    
+    return 0;
+}
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
+/**
+  * @brief  All initialisation.
+  * @param  None
+  * @retval NULL
+  */
+static void Init(void)
+{
+
+}
+
+/**
+  * @brief  All settings.
+  * @param  None
+  * @retval NULL
+  */
+static void Config(void)
+{
+
+}
+
+
+
+/**
+  * @brief  Program loop.
+  * @param  None
+  * @retval NULL
+  */
+static void Loop(void)
+{
+    for(;;) {
+
+    }
+}
+
+/******************************* (END OF FILE) *********************************/
+
+   
